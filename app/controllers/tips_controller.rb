f109a8c26542bfc7d8cc35462a6c18b34cd38ea3
NehemiahAndrews <nehemiah.andrews@gmail.com>
2017-07-03 00:17:56 +0000

    Added create function

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -6,4 +6,15 @@ class TipsController < ApplicationController
     def new
       @tip = Tip.new
     end
+
+    def create
+      Tip.create(tip_params)
+      redirect_to root_path
+    end
+
+    private
+
+    def tip_params
+      params.require(:tip).permit(:advice, :author)
+    end
 end
